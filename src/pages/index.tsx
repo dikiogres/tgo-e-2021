c1b257f0504751488063e9697cff1efc94c1dd6b
dikiogres <dikiogres@gmail.com>
2023-05-04 00:12:30 +0700

    feat: mapping over bugs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,39 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import * as React from 'react';
 
 import Layout from '@/components/layout/Layout';
 import Seo from '@/components/Seo';
 
+import { getBugs } from '../../sanity/sanity-utils';
+
 export default function HomePage() {
+  const [loading, setLoading] = React.useState(true);
+  const [bugs, setBugs] = React.useState([]);
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const data = await getBugs();
+      setBugs(data);
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  // console.log(bugs)
+
   return (
     <Layout>
       <Seo />
       <main>
-        <section className='bg-white'></section>
+        <section className='bg-white'>
+          <div>
+            {loading ? (
+              <p>Loading...</p>
+            ) : (
+              bugs.map((bug: any) => <div key={bug._id}>{bug.name}</div>)
+            )}
+          </div>
+        </section>
       </main>
     </Layout>
   );
