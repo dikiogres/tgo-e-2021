da065ae51c2d7756bf86c0828bed55c011e9a11f
dikiogres <dikiogres@gmail.com>
2023-05-04 02:19:45 +0700

    feat: navbar

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 
-// import Header from '@/components/layout/Header';
+import Header from '@/components/layout/Header';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   // Put Header or Footer Here
   return (
-    <div className='layout pt-20'>
-      {/* <Header/> */}
+    <div className='layout mx-auto max-w-5xl  pt-20'>
+      <Header />
 
       <main>{children}</main>
     </div>
