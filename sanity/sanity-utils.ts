e7324daa4ce31b85dffad99de43aaabbcd37500d
dikiogres <dikiogres@gmail.com>
2023-05-04 02:03:10 +0700

    feat: get bug groq query

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,6 +1,8 @@
 import { createClient, groq } from 'next-sanity';
 import { Bug } from 'types/Bug';
 
+import clientConfig from './config/client-config';
+
 export async function getBugs(): Promise<Bug[]> {
   const client = createClient({
     projectId: '34kbkw0y',
@@ -18,5 +20,20 @@ export async function getBugs(): Promise<Bug[]> {
         url,
         content
       }`
+  );
+}
+
+export async function getBug(slug: string): Promise<Bug> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "bug" && slug.current == $slug][0]{
+      _id,
+      _createdAt,
+      name,
+      "slug": slug.current,
+      "image": image.asset->url,
+      url,
+      content
+    }`,
+    { slug }
   );
 }
