77a6614bd73de1b7ccfc969fca112eb631ec235b
dikiogres <dikiogres@gmail.com>
2023-05-04 00:33:33 +0700

    feat: styling over data mapping

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+import Image from 'next/image';
+import Link from 'next/link';
 import * as React from 'react';
 
 import Layout from '@/components/layout/Layout';
@@ -20,18 +21,44 @@ export default function HomePage() {
     fetchData();
   }, []);
 
-  // console.log(bugs)
-
   return (
     <Layout>
       <Seo />
       <main>
         <section className='bg-white'>
-          <div>
+          <div className='mx-auto max-w-5xl py-20'>
+            <h1 className='text-7xl font-extrabold'>
+              <span className='bg-gradient-to-r from-blue-400 via-red-500 to-yellow-600 bg-clip-text text-transparent'>
+                Distributed Properties
+              </span>
+            </h1>
+            <p className='mt-3 text-xl text-gray-600'>Bug List Showcase</p>
+            <h2 className='mt-24 text-3xl font-bold text-gray-700'>Ray</h2>
             {loading ? (
               <p>Loading...</p>
             ) : (
-              bugs.map((bug) => <div key={bug._id}>{bug.name}</div>)
+              <div className='mt-5 grid gap-8 md:grid-cols-2 lg:grid-cols-3'>
+                {bugs.map((bug) => (
+                  <Link
+                    href={`/bugs/${bug.slug}`}
+                    key={bug._id}
+                    className='rounded-lg border-2 border-gray-500 p-1 transition hover:scale-105 hover:border-blue-500'
+                  >
+                    {bug.image && (
+                      <Image
+                        src={bug.image}
+                        alt={bug.name}
+                        width={750}
+                        height={300}
+                        className='rounded-lg border border-gray-500 object-cover'
+                      />
+                    )}
+                    <div className='mt-2 bg-gradient-to-r from-red-400 via-blue-500 to-yellow-600 bg-clip-text font-extrabold text-transparent'>
+                      {bug.name}
+                    </div>
+                  </Link>
+                ))}
+              </div>
             )}
           </div>
         </section>
