e369913286a4d7a26b28011c6fbe81630b8ba1bc
dikiogres <dikiogres@gmail.com>
2023-05-04 02:19:45 +0700

    feat: navbar

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,31 +1,40 @@
+import Link from 'next/link';
 import * as React from 'react';
+import { FcHome } from 'react-icons/fc';
+import { SiNotion } from 'react-icons/si';
 
 import UnstyledLink from '@/components/links/UnstyledLink';
 
 const links = [
-  { href: '/', label: 'Route 1' },
-  { href: '/', label: 'Route 2' },
+  {
+    href: 'https://eastern-power-432.notion.site/418f5d8a0be449798f313ab458357e93?v=4ff5291447c64812ad1aeaa825691c1c',
+    label: <SiNotion />,
+  },
+  // { href: '/', label: 'Route 2' },
 ];
 
 export default function Header() {
   return (
-    <header className='sticky top-0 z-50 bg-white'>
-      <div className='layout flex h-14 items-center justify-between'>
-        <UnstyledLink href='/' className='font-bold hover:text-gray-600'>
-          Home
-        </UnstyledLink>
-        <nav>
-          <ul className='flex items-center justify-between space-x-4'>
-            {links.map(({ href, label }) => (
-              <li key={`${href}${label}`}>
-                <UnstyledLink href={href} className='hover:text-gray-600'>
-                  {label}
-                </UnstyledLink>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
+    <header className='flex items-center justify-between'>
+      <Link href='/' className='flex'>
+        <h5
+          className='bg-gradient-to-r from-red-300 via-red-500 to-red-700 bg-clip-text text-2xl
+        font-bold text-transparent'
+        >
+          <FcHome />
+        </h5>
+      </Link>
+      <nav>
+        <ul className='flex items-center justify-between space-x-4'>
+          {links.map(({ href, label }) => (
+            <li key={`${href}${label}`}>
+              <UnstyledLink href={href} className='hover:text-gray-600'>
+                {label}
+              </UnstyledLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }
